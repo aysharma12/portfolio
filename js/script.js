0baa7857bedce3b644ab988b872c9a064811d1c9
aysharma12 <103080895+aysharma12@users.noreply.github.com>
2024-06-27 15:12:55 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('resume-modal');
+    const btns = document.querySelectorAll('.resume-link'); // Select all resume-link elements
+    const span = document.querySelector('.close');
+
+    // When the user clicks any resume button, open the modal
+    btns.forEach(btn => {
+        btn.onclick = function(event) {
+            modal.style.display = "block";
+        }
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
